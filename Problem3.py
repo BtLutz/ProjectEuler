429f38c770fcc5093f6478cceb4528b272985788
BtLutz <b@btlutz.com>
2016-01-01 12:22:02 -0500

    Completed Problem 5 w/ a little help from StackOverflow

--- Problem3.py
+++ Problem3.py
@@ -2,6 +2,16 @@ import pdb
 import math
 num = 600851475143 
 primeFactors = []
+k = 2
+
+while num > 1:
+    if num % k == 0:
+        print k
+        while num % k == 0:
+            num = num / k
+    k += 1
+
+# First version
 for i in range(2, int(math.sqrt(num))):
     print i
     prime = True
@@ -11,7 +21,6 @@ for i in range(2, int(math.sqrt(num))):
             break 
     if prime == True:
         if num % i == 0:
-            num = num / i
             primeFactors.append(i)
             print primeFactors
 print primeFactors
