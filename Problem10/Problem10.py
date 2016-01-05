71dfaefd14545d1ead90e4e7c02a8243389f17a3
BtLutz <b@btlutz.com>
2016-01-04 21:05:41 -0500

    Finished Problem 10. Trying to get Nerdcommenter to work in ViM. Help?

---
+++ Problem10/Problem10.py
@@ -0,0 +1,32 @@
+from sys import argv
+
+def factors(n):
+    # Deprecated
+    return set(reduce(list.__add__,
+                            ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+
+
+def is_prime(x):
+    # Deprecated
+   return factors(x) == set([1, x])
+   
+def find_primes_to(k):
+    
+    prime_set = set()
+    for i in range(2, k+1):
+        if i not in prime_set:
+            yield i
+            prime_set.update(range(i**2, k+1, i))
+    
+
+    ##### First Attempt #####
+   #sum = 2
+   #for i in range(3,k,+2):
+   #    print i
+   #    if is_prime(i):
+   #        sum += i
+   #return sum
+
+if __name__ == "__main__":
+    print sum(list(find_primes_to(int(argv[1]))))
+
